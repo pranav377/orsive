d35cc73b9665118a56eb8619257c146aa0b281f6
Pranava <pranavamohan321@gmail.com>
2022-05-17 18:42:40 +0530

    Offline handling

--- apps/web/app/store/reducers/app/cases.ts
+++ apps/web/app/store/reducers/app/cases.ts
@@ -3,6 +3,7 @@ const APP_CASES = {
   HIDE_BARS: "app/HIDE_BARS",
   SHOW_REPLY: "app/SHOW_REPLY",
   HIDE_REPLY: "app/HIDE_REPLY",
+  SET_OFFLINE: "app/SET_OFFLINE",
 };
 
 export default APP_CASES;
