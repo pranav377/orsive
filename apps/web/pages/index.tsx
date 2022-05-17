9229ef39b5aea22bec70749d2cac75c2481df83e
Pranava <pranavamohan321@gmail.com>
2022-05-17 15:18:53 +0530

    PWA test

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,9 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Section1 from "../components/home/section1";
 import Section2 from "../components/home/section2";
 
 import "animate.css/animate.min.css";
 import { Layout } from "../components/app/Layout";
-import { USER_COOKIE_KEY } from "../config";
 import { useAuthRedirect } from "../hooks/app/useAuthRedirect";
 
 const Home: NextPage = () => {
@@ -20,25 +19,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-function getCook(cookiename: string, cookieString: string) {
-  var cookiestring = RegExp(cookiename + "=[^;]+").exec(cookieString);
-  return decodeURIComponent(
-    !!cookiestring ? cookiestring.toString().replace(/^[^=]+./, "") : ""
-  );
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  if (getCook(USER_COOKIE_KEY, req.headers.cookie!) === "true") {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/feed",
-      },
-      props: {},
-    };
-  }
-  return {
-    props: {},
-  };
-};
