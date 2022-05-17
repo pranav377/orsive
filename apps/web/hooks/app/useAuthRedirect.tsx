30c7ceb6c49900ad6d0be44c553d095435216c03
Pranava <pranavamohan321@gmail.com>
2022-05-17 15:33:40 +0530

    Temporary offline fix

--- apps/web/hooks/app/useAuthRedirect.tsx
+++ apps/web/hooks/app/useAuthRedirect.tsx
@@ -1,11 +1,25 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { USER_COOKIE_KEY } from "../../config";
 import { useUser } from "../auth/useUser";
 
 export const useAuthRedirect = () => {
   const user = useUser();
   const router = useRouter();
 
-  if (router.pathname !== "/feed" && user.is) {
-    router.push("/feed");
-  }
+  useEffect(() => {
+    if (
+      router.pathname !== "/feed" &&
+      (user.is || getCook(USER_COOKIE_KEY, document.cookie) === "true")
+    ) {
+      router.push("/feed");
+    }
+  });
 };
+
+function getCook(cookiename: string, cookieString: string) {
+  var cookiestring = RegExp(cookiename + "=[^;]+").exec(cookieString);
+  return decodeURIComponent(
+    !!cookiestring ? cookiestring.toString().replace(/^[^=]+./, "") : ""
+  );
+}
