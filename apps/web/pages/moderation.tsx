3d11db24e0d2e58067c96c721f496f945cd7ddca
Pranava <pranavamohan321@gmail.com>
2022-06-03 15:25:04 +0530

    Added message for unauthenticated users in moderation bay

--- apps/web/pages/moderation.tsx
+++ apps/web/pages/moderation.tsx
@@ -2,19 +2,50 @@ import VirtualScroller from "virtual-scroller/react";
 import { Layout } from "../components/app/Layout";
 import OneTimePageSpinner from "../components/app/OneTimePageSpinner";
 import Spinner from "../components/app/Spinner";
+import AccessDenied from "../components/forms/content/accessDenied";
 import ModerationImagePostCard from "../components/moderation/ModerationImagePostCard";
 import ModerationOrsicPostCard from "../components/moderation/ModerationOrsicPostCard";
 import { useModeration } from "../hooks/app/moderation/useModeration";
 import { useOneTimePageSpinner } from "../hooks/app/useOneTimePageSpinner";
+import NotifySVG from "../components/svgs/notify.svg";
 
 export default function Moderation() {
-  const { query, loadMoreElement } = useModeration();
+  const { user, query, loadMoreElement } = useModeration();
   const { spinnerShown, setSpinnerShown } = useOneTimePageSpinner(query.data);
 
   return (
     <>
       <Layout title={"Moderation Bay | Orsive"}>
         <div className="flex items-center mb-1 flex-col">
+          {!user.is && (
+            <div className="h-[60vh] md:h-[90vh] flex">
+              <AccessDenied />
+            </div>
+          )}
+
+          {user.is && !user.isMod && (
+            <div className="h-[60vh] md:h-[80vh] flex">
+              <div className="flex flex-col self-center w-full h-full justify-center">
+                <div className="w-full flex items-center justify-center">
+                  <NotifySVG className="w-full max-w-sm" />
+                </div>
+                <div className="flex flex-col m-2 max-w-xl self-center">
+                  <span className="font-semibold text-2xl m-1">
+                    You are not a Mod
+                  </span>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {user.is && user.isMod && (
+            <OneTimePageSpinner
+              long
+              data={query.data}
+              spinnerShown={spinnerShown}
+            />
+          )}
+
           {query.data && (
             <>
               <VirtualScroller
@@ -52,12 +83,6 @@ export default function Moderation() {
               )}
             </>
           )}
-
-          <OneTimePageSpinner
-            long
-            data={query.data}
-            spinnerShown={spinnerShown}
-          />
         </div>
       </Layout>
     </>
