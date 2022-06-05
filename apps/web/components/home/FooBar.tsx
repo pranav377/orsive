6ee0bab2622912fa095a2f0823c7e9efd6f55a0a
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-06-05 16:08:45 +0530

    Added github link in footbar

--- apps/web/components/home/FooBar.tsx
+++ apps/web/components/home/FooBar.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 export default function FooBar() {
   return (
     <footer className="text-center bg-gray-900 text-white absolute bottom-0 w-full">
       <div
-        className="text-center p-4"
+        className="text-center p-2"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
+        <div className="flex gap-1 justify-center mb-2">
+          <Link href="https://github.com/pranav377/orsive" passHref>
+            <a className="text-blue-700 hover:underline hover:text-blue-800 ">
+              Github
+            </a>
+          </Link>
+        </div>
         © Orsive
       </div>
     </footer>
