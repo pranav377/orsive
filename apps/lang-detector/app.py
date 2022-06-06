b56a013d6cd18dd4efa1b70ffb4a3745b85dd6a2
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-06-06 15:28:52 +0530

    Lang detector V1 ready

--- apps/lang-detector/app.py
+++ apps/lang-detector/app.py
@@ -1,8 +1,25 @@
+import joblib
 from fastapi import FastAPI
+from pydantic import BaseModel
+from sklearn.preprocessing._label import LabelEncoder
+from sklearn.svm._classes import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer:TfidfVectorizer = joblib.load('./vectorizer.sav')
+model:SVC = joblib.load('./languageModel.model')
+encoder:LabelEncoder = joblib.load('./label.encoder')
 
 app = FastAPI()
-
 
 @app.get("/")
 def root():
     return "Lang Detection server up and running"
+
+class DetectArgs(BaseModel):
+    content: str
+@app.post("/detect/")
+def detect(args: DetectArgs):
+    content = vectorizer.transform([args.content])
+    result = encoder.inverse_transform(model.predict(content))[0]
+
+    return {"result": result}
