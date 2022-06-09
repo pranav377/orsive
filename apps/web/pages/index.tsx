7c3fb65bde01c5c841ed2f13874b0cb905c4d581
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-06-09 18:54:50 +0530

    Creating mobile app for orsive

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import { Layout } from "../components/app/Layout";
 import { useHome } from "../hooks/pages/useHome";
 import Spinner from "../components/app/Spinner";
-import Hero from "../components/home/Hero";
+// import Hero from "../components/home/Hero";
 import LogoSVG from "../components/svgs/logo.svg";
 import HomeButtons from "../components/home/HomeButtons";
 import FooBar from "../components/home/FooBar";
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
         ) : (
           <>
             <div className="relative h-screen -mb-24">
-              <Hero />
+              {/* <Hero /> */}
               <div className="w-full flex flex-col items-center mt-20 mb-20 home">
                 <LogoSVG className="w-32 floating" />
                 <span className="text-4xl text-center text-gray-200 font-semibold w-8/12">
