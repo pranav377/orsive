3f140d58eb26c620b79d5a801afac189f163ec97
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-06-09 15:56:02 +0530

    Trying to fix client side error on ssr

--- apps/web/components/home/Hero.tsx
+++ apps/web/components/home/Hero.tsx
@@ -3,19 +3,20 @@ import { loadSnowPreset } from "tsparticles-preset-snow";
 
 export default function Hero() {
   return (
-    <Particles
-      id="tsparticles"
-      init={async (engine) => {
-        await loadSnowPreset(engine);
-      }}
-      options={{
-        preset: "snow",
-        background: {
-          color: "transparent",
-        },
-      }}
-    >
-      <div className="w-full h-full bg-blue-700">hellos</div>
-    </Particles>
+    // <Particles
+    //   id="tsparticles"
+    //   init={async (engine) => {
+    //     await loadSnowPreset(engine);
+    //   }}
+    //   options={{
+    //     preset: "snow",
+    //     background: {
+    //       color: "transparent",
+    //     },
+    //   }}
+    // >
+    //   <div className="w-full h-full bg-blue-700"></div>
+    // </Particles>
+    <></>
   );
 }
