a2cf347d6b458c64d56eec2e330fce681ffc8d79
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-06-10 06:46:48 +0530

    Added sentry for error monitoring

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,12 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
+const { withSentryConfig } = require("@sentry/nextjs");
 
-const nextConfig = {
+const moduleExports = withPWA({
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
@@ -26,6 +31,12 @@ const nextConfig = {
     cacheOnFrontEndNav: true,
     dynamicStartUrlRedirect: true,
   },
+});
+
+const sentryWebpackPluginOptions = {
+  silent: true,
 };
 
-module.exports = withPWA(nextConfig);
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
