d936690440ff9b8f7b0dc014e06c1fcff3237b44
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-07-28 17:19:13 +0530

    Editor issue fixed

--- apps/mobile-app/src/screens/SignedInStack/PostContentScreen/PostOrsic.tsx
+++ apps/mobile-app/src/screens/SignedInStack/PostContentScreen/PostOrsic.tsx
@@ -1,72 +1,27 @@
-import { Tailwind } from "@jeact/colors";
-import { useState } from "react";
-import { Image, TextInput, View } from "react-native";
-import { Text } from "react-native-paper";
-import { RFValue } from "react-native-responsive-fontsize";
-import { PhotographIcon } from "react-native-heroicons/solid";
-import * as ImagePicker from "expo-image-picker";
-
-interface Block {
-  name: "paragraph" | "image";
-  value: string;
-  number: number;
-  data?: any;
-}
+import { useRef } from "react";
+import { RichEditor } from "react-native-pell-rich-editor";
 
 export default function PostOrsic() {
-  const [allEditorBlocks, setEditorBlocks] = useState<Array<Block>>([]);
-  const [lastIndex, setLastIndex] = useState(0);
+  const editor = useRef<RichEditor>(null);
 
   return (
-    <View style={{ padding: RFValue(5), flex: 1 }}>
-      <TextInput
-        multiline
-        placeholder="Write here"
-        placeholderTextColor={Tailwind.gray[400]}
-        style={{ color: "white" }}
-        autoFocus
-        onChangeText={(text) => {
-          let allBlocks: Array<Block> = [];
-          let allText = text.split(/(\n)/g);
-
-          allText.map((oldText) => {
-            setLastIndex((prevIndex) => {
-              allBlocks.push({
-                name: "paragraph",
-                value: oldText,
-                number: prevIndex + 1,
-              });
-
-              return prevIndex + 1;
-            });
-          });
-
-          setEditorBlocks(allBlocks);
+    <>
+      <RichEditor
+        disabled={false}
+        editorStyle={{
+          backgroundColor: "black",
+          color: "white",
+          placeholderColor: "white",
         }}
-      >
-        {allEditorBlocks.map((block, idx) => {
-          return (
-            <Text key={idx} style={{ color: "white" }}>
-              {block.value}
-            </Text>
-          );
-        })}
-      </TextInput>
-      <View style={{ marginTop: "auto", padding: RFValue(5) }}>
-        <PhotographIcon
-          onPress={async () => {
-            let result = await ImagePicker.launchImageLibraryAsync({
-              mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            });
-
-            if (!result.cancelled) {
-              const response = await fetch(result.uri);
-              const image = await response.blob();
-            }
-          }}
-          color="white"
-        />
-      </View>
-    </View>
+        ref={editor}
+        style={{
+          minHeight: 300,
+          flex: 1,
+        }}
+        initialHeight={400}
+        placeholder={"What do you think?"}
+        pasteAsPlainText={true}
+      />
+    </>
   );
 }
