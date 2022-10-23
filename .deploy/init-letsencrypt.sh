8e491d7846c6d8444a1c8556d83bcea2aa6fb9fd
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-10-23 11:25:57 +0000

    Fix config

--- .deploy/init-letsencrypt.sh
+++ .deploy/init-letsencrypt.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-if ! [ -x "$(command -v docker compose)" ]; then
-  echo 'Error: docker compose is not installed.' >&2
-  exit 1
-fi
-
 domains=(api.orsive.com search.orsive.com)
 rsa_key_size=4096
 data_path="./data/certbot"
