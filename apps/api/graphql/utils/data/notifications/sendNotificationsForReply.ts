c5ed7576c39b8082151a3389ff475e7744cf6ede
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2022-12-17 09:15:31 +0000

    Going to change notifications infrastructure

--- apps/api/graphql/utils/data/notifications/sendNotificationsForReply.ts
+++ apps/api/graphql/utils/data/notifications/sendNotificationsForReply.ts
@@ -1,48 +1,18 @@
 // send notification to the comment owner when he/she gets a reply on their comment. Send only if owner and reply owner are not same
 
-import { NODE_ENV } from "../../../config";
 import prisma from "../dbClient";
-import NotificationClient from "./client";
 
 const THRESHOLD = 100;
 
 async function sendNotification(
   commentOwnerId: string,
   replyId: string,
-  replyUploadedById: string,
+  _replyUploadedById: string,
   percentage: number,
-  url: string
+  _url: string
 ) {
   var random = Math.floor(Math.random() * 101);
   if (random <= percentage) {
-    let commentOwner = await prisma.profile.findUnique({
-      where: {
-        id: commentOwnerId,
-      },
-    });
-
-    let repliedBy = await prisma.profile.findUnique({
-      where: {
-        id: replyUploadedById,
-      },
-    });
-
-    if (NODE_ENV === "production") {
-      await Promise.all(
-        commentOwner!.notificationToken?.map(async (notificationToken) => {
-          await NotificationClient.post("", {
-            to: notificationToken,
-            data: {
-              title: "New Reply",
-              body: `${repliedBy!.name} replied on your comment`,
-              for: commentOwner!.username,
-              url,
-            },
-          });
-        })
-      );
-    }
-
     return prisma.notificationForComment.create({
       data: {
         commentId: replyId,
