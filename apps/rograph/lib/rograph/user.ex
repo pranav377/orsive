3811b6f071df2614bcda1bc38df7548ced2d99b7
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2023-04-21 15:17:55 +0000

    changed user ecto schema

--- apps/rograph/lib/rograph/user.ex
+++ apps/rograph/lib/rograph/user.ex
@@ -2,6 +2,9 @@ defmodule Rograph.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Rograph.User
+  alias Rograph.Relationship
+
   @primary_key {:id, :string, []}
   schema "users" do
     field(:username, :string)
@@ -10,10 +13,25 @@ defmodule Rograph.User do
     field(:avatar, :string)
     field(:banner, :string)
     field(:bio, :string)
-    field(:is_online, :boolean, default: false)
+    field(:last_active, :utc_datetime)
     field(:joined, :utc_datetime)
     many_to_many(:channels, Rograph.Chat.Channel, join_through: "users_channels")
     many_to_many(:typing_channels, Rograph.Chat.Channel, join_through: "typing_users_channels")
+
+    many_to_many(
+      :blocked_users,
+      User,
+      join_through: Relationship,
+      join_keys: [user_id: :id, relation_id: :id]
+    )
+
+    many_to_many(
+      :reverse_blocked_users,
+      User,
+      join_through: Relationship,
+      join_keys: [relation_id: :id, user_id: :id]
+    )
+
     has_many(:read_messages, Rograph.Chat.MessageRead)
   end
 
