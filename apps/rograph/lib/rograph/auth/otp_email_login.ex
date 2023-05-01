b72411955fe78fc8dfa5f497ca50a5df925a54eb
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2023-05-01 06:39:05 +0000

    add create otp email login object

--- apps/rograph/lib/rograph/auth/otp_email_login.ex
+++ apps/rograph/lib/rograph/auth/otp_email_login.ex
@@ -9,29 +9,14 @@ defmodule Rograph.Auth.OtpEmailLogin do
     belongs_to(:user, Rograph.Auth.User, type: :string)
 
     timestamps()
-
-    # validation virtual fields
-    field(:user_id, :string, virtual: true)
   end
 
   @doc false
   def changeset(channel, attrs) do
     channel
-    |> cast(attrs, [:otp, :user_id])
-    |> validate_required([:otp, :user_id])
+    |> cast(attrs, [:otp, :user])
+    |> validate_required([:otp, :user])
     |> validate_length(:otp, is: 7)
     |> add_user()
-  end
-
-  defp add_user(changeset) do
-    user_id = get_change(changeset, :user_id)
-
-    case Repo.get(User, user_id) do
-      nil ->
-        add_error(changeset, :user_id, "user does not exist")
-
-      user ->
-        put_assoc(changeset, :user, user)
-    end
   end
 end
--- apps/rograph/lib/rograph_web/graphql/resolvers/auth_resolver.ex
+++ apps/rograph/lib/rograph_web/graphql/resolvers/auth_resolver.ex
@@ -1,24 +1,34 @@
 defmodule RographWeb.Graphql.Resolvers.AuthResolver do
   alias Rograph.Repo
   alias Rograph.Auth.User
+  alias(Rograph.Auth.OtpEmailLogin)
 
   def send_auth_otp(_parent, %{email: email}, _context) do
     case Repo.get_by(User, email: email) do
       # signup process
       nil ->
         IO.puts("signup")
-        {:ok, %{}}
+        {:ok, %{type: "signup"}}
 
       # login process
       user ->
         # generate 7 digit otp
         otp = generate_otp()
 
-        {:ok, %{}}
+        %OtpEmailLogin{}
+        |> OtpEmailLogin.changeset(%{
+          otp: otp,
+          user: user
+        })
+        |> Repo.insert()
+
+        # send otp to email
+
+        {:ok, %{type: "login"}}
     end
   end
 
-  def generate_otp() do
+  defp generate_otp() do
     # Generate a random 7-digit number
     otp = Enum.random(1_000_000..9_999_999)
 
